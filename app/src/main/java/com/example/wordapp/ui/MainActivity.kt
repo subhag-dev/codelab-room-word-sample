ae5373dbe8823d95b05a0f8708739ba6146a5806
subhag-dev <subhag.dev@gmail.com>
2024-01-23 21:37:43 +0530

    Create word entity

--- app/src/main/java/com/example/wordapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/wordapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.wordapp
+package com.example.wordapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wordapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
